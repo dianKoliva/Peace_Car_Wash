d7f06bf372b231743ebb3c27cbd7371523cfc31e
pretty123dian <prettyntakirutimana@gmail.com>
2021-08-01 20:14:31 +0300

    new canges

--- src/components/VehichlePayment.js
+++ src/components/VehichlePayment.js
@@ -1,7 +1,114 @@
+import { Grid, TextField, Typography, makeStyles } from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  width: {
+    width: "70%",
+  },
+  margin: {
+    marginLeft: "10em",
+    marginTop: "10em",
+  },
+  greenBut: {
+    backgroundColor: "#4AAF05",
+    color: "white",
+  },
+  blueBut: {
+    backgroundColor: "#1665D8",
+    color: "white",
+  },
+  inputmag: {
+    marginTop: "10px",
+  },
+}));
 function VehichlePayment(props) {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid xs="12" container spacing={3}>
+        <Grid item xs={6}>
+          <Typography variant="h6">Service Payment</Typography>
+        </Grid>
+        <Grid container xs="12" className={classes.margin}>
+          <Grid item xs="12">
+            <p className="text-lg font-bold">Pay for vehicle services</p>
+          </Grid>
+          <Grid item xs="6" className={classes.inputmag}>
+            <TextField
+              margin="dense"
+              label="Amount"
+              variant="outlined"
+              size="small"
+              className={classes.width}
+            />
+            <br></br>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked=""
+                  onChange=""
+                  name="checkedB"
+                  color="primary"
+                />
+              }
+              label="Accept Terms and Conditions"
+            />
+          </Grid>
+
+          <Grid item xs="6">
+            <TextField
+              margin="dense"
+              id="date"
+              label="Payement Date"
+              variant="outlined"
+              type="date"
+              defaultValue="2017-05-24"
+              size="small"
+              className={classes.width}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <br></br>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked=""
+                  onChange=""
+                  name="checkedB"
+                  color="primary"
+                />
+              }
+              label="Completed Payment"
+            />
+          </Grid>
+          <Grid xs={12} container spacing={3} className={classes.inputmag}>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={`${classes.blueBut} ${classes.width} ${classes.low}`}
+              >
+                Save
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={`${classes.greenBut} ${classes.width} ${classes.low}`}
+              >
+                Continue Payment
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default VehichlePayment;
