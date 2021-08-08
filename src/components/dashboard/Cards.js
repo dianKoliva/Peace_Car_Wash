4d5a138306870b6334083a1fe1b6f631c388d72a
pretty123dian <prettyntakirutimana@gmail.com>
2021-08-08 22:06:37 +0300

    new changes

--- src/components/dashboard/Cards.js
+++ src/components/dashboard/Cards.js
@@ -1,17 +1,39 @@
 import { Grid ,Paper} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+
+    margin:{
+        marginLeft:'20px',
+        marginTop:'20px',
+        marginBottom:'20px',
+        marginRight:'20px'
+    }
+
+}))
+
+
+
 
 export default function Cards() {
+const classes=useStyles();
   return (
     <div>
-      <Grid container xs="12">
-        <Grid container xs="4">
-            <Paper>
+      <Grid container xs="12" spacing={3}>
+        <Grid container xs="4" >
+            <Paper className={classes.margin}>
 <Grid item xs="12">
     <p>PROFITS</p>
     <div className="flex font-bold text-lg"> 
     <p>24,000</p>
     <p>Frw</p>
     </div>
+
+    <div className="flex font-bold text-md"> 
+    <p>Month:</p>
+    <p>September</p>
+    </div>
+    
     
 </Grid>
             </Paper>
