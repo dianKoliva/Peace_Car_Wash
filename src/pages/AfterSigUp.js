3e6086e147f6967ff94caeaf3114864026ee2618
= <=>
2021-08-26 20:45:47 +0300

    new changes

---
+++ src/pages/AfterSigUp.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import bg from "../images/bg.jpg";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from 'axios';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "80hv",
+    marginTop: "30px",
+  },
+  image: {
+    backgroundImage: `url(${bg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(4, 4),
+    display: "flex",
+    flexDirection: "column",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+export default function SignUpSide() {
+
+const classes=useStyles();
+
+
+  return (
+    <div className="w-2/3 h-96 ml-52">
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        
+      </Grid>
+    </div>
+  );
+}
+/* eslint-enable no-unused-vars */
