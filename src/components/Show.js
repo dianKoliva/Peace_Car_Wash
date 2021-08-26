e227569d82a3c31505138d4c91b2e5dcbc40f2a8
= <=>
2021-08-26 23:18:06 +0300

    new changes

--- src/components/Show.js
+++ src/components/Show.js
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
       width: "100%",
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
+    
     },
     nested: {
       paddingLeft: theme.spacing(4),
