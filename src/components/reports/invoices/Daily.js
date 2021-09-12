e5b52ac32af636bebc561adff2a401d8a8d0ae31
= <=>
2021-09-12 17:10:12 +0300

    new changes

---
+++ src/components/reports/invoices/Daily.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function Daily(){
+
+    return(
+        <div>
+            <table>
+  <tr>
+    <th>Data 1</th>
+    <th >Data 2</th>
+  </tr>
+  <tr>
+    <td>Calcutta</td>
+    <td >Orange</td>
+  </tr>
+  <tr>
+    <td>Robots</td>
+    <td >Jazz</td>
+  </tr>
+</table>
+        </div>
+    )
+
+
+}
+
+export default Daily;
