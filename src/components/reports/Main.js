061d576e5743bec804f8c8cb05b0f6555d651c24
= <=>
2021-09-14 07:47:13 +0300

    new changes

--- src/components/reports/Main.js
+++ src/components/reports/Main.js
@@ -66,7 +66,7 @@ export default function StickyHeadTable() {
     let d={type:type,branch:branch,from:from,to:to}
 
     setTheReport(d);
-    console.log(theReport);
+    console.log(theReport.branch);
     
       // history.push(reporter);
    
